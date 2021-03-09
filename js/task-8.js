0aa5f15198cbd480200d4584e11babad4cc8239c
Vladimir Likhachiov <likhachiovvl@gmail.com>
2021-03-09 15:29:10 +0200

    rej-8
    Rejection from 8th task

--- js/task-8.js
+++
@@ -1,6 +0,0 @@
-const refs = {
-  input: document.querySelector('number'),
-  btn: document.querySelector('[data-action="render"]'),
-  button: document.querySelector('[data-action="destroy"]'),
-};
-console.log(refs);
